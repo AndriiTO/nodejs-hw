cb297c913dd52f57fe1be5e3c4b0cda0ea0f5ee7
seedre <taocripto@gmail.com>
2026-03-15 22:03:15 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from "cors";
+import pino from "pino-http";
+import 'dotenv/config';
+import helmet from "helmet";
+
+
+const app = express();
+                                      // Middleware
+app.use(helmet());
+// Middleware CORS
+app.use(cors());
+// для парсинга JSON тіла запиту
+app.use(express.json());
+app.use(pino());
+
+// localhost.get/notes
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: "Retrieved all notes" });
+});
+
+// GET /notes/:noteId
+app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({ message: "Retrieved note with ID: id_param" });
+});
+// /test-error
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+// 404 Not Found
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// error middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message, message: "повідомлення про помилку" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000');
+});
