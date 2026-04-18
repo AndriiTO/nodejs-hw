e3aec9a2cf27f4f0d0c2e2f613df43c0be9bd56a
seedre <taocripto@gmail.com>
2026-04-18 05:24:49 +0200

    middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { updateUserAvatar } from '../controllers/userController.js';
-import { authenticate } from '../middlewares/authenticate.js';
+import { authenticate } from '../middleware/authenticate.js';
 import { upload } from '../middleware/multer.js';
 
 
